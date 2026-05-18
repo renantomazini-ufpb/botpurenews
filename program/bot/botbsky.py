120c33c4fd3a0456e32d8d4f9e7c382b4297c601
Renan UFPB <renantomazini@sti.ufpb.br>
2026-05-18 07:43:01 -0300

    retorno sem headers

--- program/bot/botbsky.py
+++ program/bot/botbsky.py
@@ -3,18 +3,17 @@ from program.scripts import geradorNew
 import time
 import os
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-}
-
-# Inicialize o cliente passando os cabeçalhos customizados
 client = Client()
-client.request.headers.update(HEADERS) # <-- Força a biblioteca a se disfarçar de navegador
 
-# O resto do seu código continua exatamente igual...
+# GitHub actions aqui
 user = os.environ.get("BSKY_USER")
 password = os.environ.get("BSKY_PASS")
+
+if not user or not password:
+    raise ValueError("Credenciais não encontradas")
+
 client.login(user, password)
+
 
 def post():
     news = geradorNew.getANews()
